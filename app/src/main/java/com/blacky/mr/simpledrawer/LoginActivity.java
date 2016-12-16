ed2c936b0660521e593e391e43cd49c0b68a3158
Dmitry <mr.blacky01@gmail.com>
2016-12-16 23:36:19 +0300

    +Add functionality to work with profile

--- app/src/main/java/com/blacky/mr/simpledrawer/LoginActivity.java
+++ app/src/main/java/com/blacky/mr/simpledrawer/LoginActivity.java
@@ -1,20 +1,39 @@
 package com.blacky.mr.simpledrawer;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Process;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button loginButton;
+    Button enterButton;
+    Button changeUserButton;
+    Button logoutButton;
     Toolbar toolbar;
+    TextView profileTextView;
+    LinearLayout actionLayout;
+
+    CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +41,97 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         loginButton = (Button) findViewById(R.id.login_button);
+        enterButton = (Button) findViewById(R.id.enter_button);
+        changeUserButton = (Button) findViewById(R.id.change_user);
+        logoutButton = (Button) findViewById(R.id.logout_button);
+        profileTextView = (TextView) findViewById(R.id.profile_info);
+        actionLayout = (LinearLayout) findViewById(R.id.action_linear_layout);
         toolbar = (Toolbar) findViewById(R.id.toolbar_login);
         setSupportActionBar(toolbar);
         toolbar.setLogo(R.mipmap.ic_launcher);
 
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        mCallbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(mCallbackManager,
+            new FacebookCallback<LoginResult>() {
+                private ProfileTracker mProfileTracker;
+                @Override
+                public void onSuccess(LoginResult loginResult) {
+
+                    if(Profile.getCurrentProfile() == null) {
+                        mProfileTracker = new ProfileTracker() {
+                            @Override
+                            protected void onCurrentProfileChanged(Profile profile, Profile profile2) {
+                                profileTextView.setText(getString(R.string.hello_string) +profile2.getName());
+                                mProfileTracker.stopTracking();
+                            }
+                        };
+                    }
+                    else {
+                        Profile profile = Profile.getCurrentProfile();
+                        profileTextView.setText(getString(R.string.hello_string) +profile.getName());
+                    }
+                    fetchProfile();
+                }
+
+                @Override
+                public void onCancel() {
+                    Toast.makeText(LoginActivity.this, "Login Cancel", Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onError(FacebookException exception) {
+                    Toast.makeText(LoginActivity.this, exception.getMessage(), Toast.LENGTH_LONG).show();
+                    LoginManager.getInstance().logOut();
+                }
+            });
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,DrawerActivity.class);
-                startActivity(intent); 
+                LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Arrays.asList("public_profile"));
             }
         });
+
+    }
+
+    private void fetchProfile(){
+        VisibilityChange(View.VISIBLE);
+        loginButton.setVisibility(View.INVISIBLE);
+
+        changeUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager manager = LoginManager.getInstance();
+                manager.logOut();
+                manager.logInWithReadPermissions(LoginActivity.this, Arrays.asList("public_profile"));
+            }
+
+        });
+
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,DrawerActivity.class);
+                startActivity(intent);
+            }
+        });
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager.getInstance().logOut();
+                VisibilityChange(View.INVISIBLE);
+                loginButton.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private void VisibilityChange(int visibility)
+    {
+        profileTextView.setVisibility(visibility);
+        actionLayout.setVisibility(visibility);
+        logoutButton.setVisibility(visibility);
     }
 
     @Override
@@ -49,6 +148,14 @@ public class LoginActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(mCallbackManager.onActivityResult(requestCode, resultCode, data)) {
+            return;
         }
     }
 
